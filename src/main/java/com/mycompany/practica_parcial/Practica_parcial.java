ca2d1f7522b15d930aeee98b9efd3c563a0669d0
bryanvanegas <bryan.steveen98@hotmail.com>
2025-08-26 15:15:00 -0500

    Se creo el proyecto

---
+++ src/main/java/com/mycompany/practica_parcial/Practica_parcial.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica_parcial;
+
+/**
+ *
+ * @author BryanVanegas
+ */
+public class Practica_parcial {
+
+    public static void main(String[] args) {
+
+
+    }
+}
