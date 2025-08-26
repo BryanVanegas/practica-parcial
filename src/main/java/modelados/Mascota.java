2766fa7137f8a12d5823b534d371365e56cee3bd
bryanvanegas <bryan.steveen98@hotmail.com>
2025-08-26 16:16:45 -0500

    ADD: Clase Mascota

---
+++ src/main/java/modelados/Mascota.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelados;
+
+/**
+ *
+ * @author BryanVanegas
+ */
+public class Mascota {
+    
+    private String raza;
+
+    public Mascota(String raza) {
+        this.raza = raza;
+    }
+    
+    
+    
+}
