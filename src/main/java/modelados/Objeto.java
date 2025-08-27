4b796bba8ae1438e6bf17e89cb5179045b6017a8
Mateo Hurtado <hurtadotorresmateo@gmail.com>
2025-08-26 23:03:24 -0500

    ADD metodos Getter y Setter

--- src/main/java/modelados/Objeto.java
+++ src/main/java/modelados/Objeto.java
@@ -15,6 +15,20 @@ public class Objeto {
         this.material = material;       
     }
     
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+    public void setMaterial(String material) {
+        this.material = material;
+    }
     
     
 }
