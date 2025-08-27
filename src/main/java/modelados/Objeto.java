a45c4d4938a459d0ab2c35f455c45fb7ce72717c
Mateo Hurtado <hurtadotorresmateo@gmail.com>
2025-08-26 22:53:29 -0500

    ADD clase Objeto

---
+++ src/main/java/modelados/Objeto.java
@@ -0,0 +1,20 @@
+
+package modelados;
+
+/**
+ *
+ * @author MATEO
+ */
+public class Objeto {
+    
+    private String color;
+    private String material;
+
+    public Mascota(String color, String material) {
+        this.color = color;
+        this.material = material;       
+    }
+    
+    
+    
+}
